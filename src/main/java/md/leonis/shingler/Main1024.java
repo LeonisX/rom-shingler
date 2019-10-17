dee9e39bbb6eef9b2915cfe207ef012316d80de0
Leonis <tv-games@mail.ru>
2019-10-17 19:03:13 +0300

    New beginning

--- src/main/java/md/leonis/shingler/Main1024.java
+++ src/main/java/md/leonis/shingler/Main1024.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.zip.CRC32;
 
@@ -12,123 +13,123 @@ public class Main1024 {
     private static final int SHINGLE_LENGTH = 8;
     private static final int SAMPLE_PROBE = 256;
 
-    @SuppressWarnings("all")
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        final File folder = new File("D:\\Downloads\\games");
-        final File shdFolder = new File("D:\\Downloads\\games\\shd");
-        final File sampleFolder256 = new File("D:\\Downloads\\games\\sample256");
-        final File sampleFolder = new File("D:\\Downloads\\games\\sample" + SAMPLE_PROBE);
+    private static final String GAMES_DIR = "D:\\Downloads\\games\\";
 
-        final File shinglesMapFile = new File("shinglesMap" + SAMPLE_PROBE);
+    private static final File SHD_FOLDER = new File(GAMES_DIR + "shd");
 
-        shdFolder.mkdir();
-        sampleFolder.mkdir();
+    private static final int SHINGLE_MAP_THRESHOLD = 128;
+
+    private static final List<Integer> SAMPLES = Arrays.asList(16, 64, 256, 1024);
+
+    private static final Map<Integer, File> SAMPLE_DIRS_MAP =
+            SAMPLES.stream().collect(Collectors.toMap(Function.identity(), s -> new File(GAMES_DIR + "sample" + s)));
+
+    private static final Map<Integer, Map<String, Set<Long>>> SHINGLE_MAP =
+            SAMPLES.stream().collect(Collectors.toMap(Function.identity(), HashMap::new));
+
+    public static void main(String[] args) throws IOException {
+        final File folder = new File(GAMES_DIR);
+
+        SAMPLE_DIRS_MAP.values().forEach(File::mkdirs);
+
         List<File> files = listFilesForFolder(folder);
 
+        generateShingles(files);
+
+        getSamples(files);
+
+        List<Name> names = files.stream().map(f -> new Name(f, false)).collect(Collectors.toList());
+
+        generateFamilies(names, 1024, 20); // generate families
+
+    }
+
+    @SuppressWarnings("all")
+    private static void generateShingles(List<File> files) throws IOException {
+
         System.out.println("Generating shingles...");
+
+        SHD_FOLDER.mkdir();
 
         for (int i = 0; i < files.size(); i++) {
             File file = files.get(i);
 
-            File shdFile = new File(shdFolder.getAbsolutePath() + File.separator + file.getName() + ".shg");
+            File shdFile = new File(SHD_FOLDER.getAbsolutePath() + File.separator + file.getName() + ".shg");
 
             if (shdFile.exists() && shdFile.length() > 0) {
                 System.out.println(String.format(Locale.US, "Skipping: %s: %2.2f", file.getName(), ((i + 1) * 100.0 / files.size())));
                 continue;
             }
 
-            byte[] bytes = readFromFile(file);
-            Set<Long> shingles = toShingles(bytes);
+            Set<Long> shingles = toShingles(readFromFile(file));
 
             System.out.println(String.format(Locale.US, "%s: %2.2f%%", file.getName(), ((i + 1) * 100.0 / files.size())));
-            FileOutputStream fout = new FileOutputStream(shdFile);
-            ObjectOutputStream oos = new ObjectOutputStream(fout);
-            oos.writeObject(shingles);
-            oos.close();
-            fout.close();
+            serialize(shdFile, shingles);
+
+            SAMPLE_DIRS_MAP.entrySet().forEach(e -> {
+                int index = e.getKey();
+                Set<Long> filteredShingles = shingles.stream().filter(s -> s % index == 0).collect(Collectors.toSet());
+
+                if (index > SHINGLE_MAP_THRESHOLD) {
+                    SHINGLE_MAP.get(index).put(file.getName(), filteredShingles);
+                }
+
+                File sampleFile = new File(SAMPLE_DIRS_MAP.get(index).getAbsolutePath() + File.separator + file.getName() + ".shg");
+                if (!sampleFile.exists()) {
+                    serialize(sampleFile, shingles.stream().filter(s -> s % index == 0).collect(Collectors.toSet()));
+                }
+            });
         }
+    }
 
-        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("result.csv", true)));
+    @SuppressWarnings("all")
+    private static void getSamples(List<File> files) throws IOException {
+        System.out.println("\nGetting samples from disk...");
 
-        System.out.println("\nCalculating diff...");
+        SHINGLE_MAP.forEach((key, value) -> {
+            int index = key;
+            File shinglesMapFile = new File("shinglesMap" + SAMPLE_PROBE);
+            Map<String, Set<Long>> shinglesMap = (!shinglesMapFile.exists()) ? new HashMap<>() : readShingleMapFromFile(shinglesMapFile);
+            if (index > SHINGLE_MAP_THRESHOLD && shinglesMap.isEmpty()) {
+                for (int i = 0; i < files.size(); i++) {
+                    File file = files.get(i);
 
-        /*int k = 0;
-        for (int i = 0; i < files.size() - 1; i++) {
-            File file1 = files.get(i);
-            Set<Long> s1Set = readShdFromFile(new File(shdFolder.getAbsolutePath() + File.separator + file1.getName() + ".shg"));
+                    /*if (sampleFile.exists() && sampleFile.length() > 0) {
+                        System.out.println(String.format(Locale.US, "Skipping: %s: %2.2f", file.getName(), ((i + 1) * 100.0 / files.size())));
+                        continue;
+                    }*/
 
-            for (int j = i + 1; j < files.size(); j++) {
+                    System.out.println(String.format(Locale.US, "%s: %2.2f%%", file.getName(), ((i + 1) * 100.0 / files.size())));
 
-                File file2 = files.get(j);
-                System.out.println(String.format(Locale.US, "%s: %2.4f%%", file1.getName() + " -- " + file2.getName(), (k * 100.0 / (files.size() * files.size() / 2))));
+                    File srcSampleFolder = (index == SAMPLES.get(0)) ? SHD_FOLDER : SAMPLE_DIRS_MAP.get(index - 1);
 
-                Set<Long> s2Set = readShdFromFile(new File(shdFolder.getAbsolutePath() + File.separator + file2.getName() + ".shg"));
+                    Set<Long> shingles = readShdFromFile(new File(srcSampleFolder.getAbsolutePath() + File.separator + file.getName() + ".shg"));
+                    Set<Long> filteredShingles = shingles.stream().filter(s -> s % index == 0).collect(Collectors.toSet());
 
-                Set<Long> s1intersect = new HashSet<>(s1Set);
-                Set<Long> s1union = new HashSet<>(s1Set);
+                    shinglesMap.put(file.getName(), filteredShingles);
 
-                s1intersect.retainAll(s2Set);
-                s1union.addAll(s2Set);
-                double relative = s1intersect.size() * 100.0 / s1Set.size();
-                double jakkard = s1intersect.size() * 100.0 / s1union.size();
+                    File sampleFile = new File(SAMPLE_DIRS_MAP.get(index).getAbsolutePath() + File.separator + file.getName() + ".shg");
+                    if (!sampleFile.exists()) {
+                        serialize(sampleFile, shingles.stream().filter(s -> s % SAMPLE_PROBE == 0).collect(Collectors.toSet()));
+                    }
+                }
 
-                Result result = new Result(file1.getName(), file2.getName(), relative, jakkard);
-                out.println(result.toString());
-                out.flush();
-                k++;
+                serialize(shinglesMapFile, shinglesMap);
+                SHINGLE_MAP.replace(index, shinglesMap);
             }
-        }*/
+        });
+    }
 
-        // sample
-
-        System.out.println("\nGetting samples...");
-
-        Map<String, Set<Long>> shinglesMap = (!shinglesMapFile.exists()) ? new HashMap<>() : readShingleMapFromFile(shinglesMapFile);
-
-        if (!shinglesMapFile.exists()) {
-
-            for (int i = 0; i < files.size(); i++) {
-                File file = files.get(i);
-
-                File sampleFile = new File(sampleFolder.getAbsolutePath() + File.separator + file.getName() + ".shg");
-
-                /*if (sampleFile.exists() && sampleFile.length() > 0) {
-                    System.out.println(String.format(Locale.US, "Skipping: %s: %2.2f", file.getName(), ((i + 1) * 100.0 / files.size())));
-                    continue;
-                }*/
-
-                System.out.println(String.format(Locale.US, "%s: %2.2f%%", file.getName(), ((i + 1) * 100.0 / files.size())));
-
-                //TODO revert
-                Set<Long> shingles = readShdFromFile(new File(sampleFolder256.getAbsolutePath() + File.separator + file.getName() + ".shg"));
-                //Set<Long> shingles = readShdFromFile(new File(shdFolder.getAbsolutePath() + File.separator + file.getName() + ".shg"));
-                Set<Long> filteredShingles = shingles.stream().filter(s -> s % SAMPLE_PROBE == 0).collect(Collectors.toSet());
-
-                shinglesMap.put(file.getName(), filteredShingles);
-
-                FileOutputStream fout = new FileOutputStream(sampleFile);
-                ObjectOutputStream oos = new ObjectOutputStream(fout);
-                oos.writeObject(shingles.stream().filter(s -> s % SAMPLE_PROBE == 0).collect(Collectors.toSet()));
-                oos.close();
-                fout.close();
-            }
-
-            FileOutputStream fout = new FileOutputStream(shinglesMapFile);
-            ObjectOutputStream oos = new ObjectOutputStream(fout);
-            oos.writeObject(shinglesMap);
-            oos.close();
-            fout.close();
-        }
-
+    private static void generateFamilies(List<Name> names, int index, int jakkardIndex) throws IOException {
 
         System.out.println("\nComparing...");
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("result.csv", true)));
 
-        int[] kx = {0};
+        Map<String, List<Name>> families = names.stream().collect(Collectors.groupingBy(Name::getCleanName));
 
-        List<Name> names = files.stream().map(f -> new Name(f, false)).collect(Collectors.toList());
+        ///TODO every family verify by SHD, drop < 80
 
-        //List<Result> results = files.parallelStream().flatMap(file1 -> {
-        /*List<Result> results = */names.stream().forEach(name1 -> {
+        names.forEach(name1 -> {
             List<Result> res = new ArrayList<>();
 
             int i = names.indexOf(name1);
@@ -139,25 +140,28 @@ public class Main1024 {
                     List<Name> toCopy = new ArrayList<>();
                     toCopy.add(name1);
 
-                    Set<Long> s1Set = shinglesMap.get(name1.getName());
-                    //Set<Long> s1Set = readShdFromFile(new File(sampleFolder.getAbsolutePath() + File.separator + file1.getName() + ".shg"));
+                    Set<Long> s1Set;
+                    if (index > SHINGLE_MAP_THRESHOLD) {
+                        s1Set = SHINGLE_MAP.get(index).get(name1.getName());
+                    } else {
+                        s1Set = readShdFromFile(new File(SAMPLE_DIRS_MAP.get(index).getAbsolutePath() + File.separator + name1.getName() + ".shg"));
+                    }
 
                     if (i != names.size() - 1) {
-                        //System.out.println(i + ": " + file1.getName());
-
                         for (int j = i + 1; j < names.size(); j++) {
 
                             Name name2 = names.get(j);
 
                             if (name2.isDone()) {
-                                kx[0]++;
                                 continue;
                             }
 
-                            //System.out.println(String.format(Locale.US, "%s: %2.4f%%", name1.getName() + " -- " + name2.getName(), (kx[0] * 100.0 / (names.size() * names.size() / 2))));
-
-                            //Set<Long> s2Set = readShdFromFile(new File(sampleFolder.getAbsolutePath() + File.separator + file2.getName() + ".shg"));
-                            Set<Long> s2Set = shinglesMap.get(name2.getName());
+                            Set<Long> s2Set;
+                            if (index > SHINGLE_MAP_THRESHOLD) {
+                                s2Set = SHINGLE_MAP.get(index).get(name2.getName());
+                            } else {
+                                s2Set = readShdFromFile(new File(SAMPLE_DIRS_MAP.get(index).getAbsolutePath() + File.separator + name2.getName() + ".shg"));
+                            }
 
                             Set<Long> s1intersect = new HashSet<>(s1Set);
                             Set<Long> s1union = new HashSet<>(s1Set);
@@ -169,17 +173,15 @@ public class Main1024 {
 
                             Result result = new Result(name1.getName(), name2.getName(), relative, jakkard);
 
-                            if (jakkard > 35) {
+                            if (jakkard > jakkardIndex) { // 35
                                 name2.setDone(true);
                                 System.out.println(result);
                                 toCopy.add(name2);
                             }
 
-
                             res.add(result);
                             out.println(result.toString());
                             out.flush();
-                            kx[0]++;
                         }
                     }
                     name1.setDone(true);
@@ -202,15 +204,99 @@ public class Main1024 {
                 }
             } else {
                 System.out.println("Skipping: " + name1.getName());
-                for (int j = i + 1; j < names.size(); j++) {
-                    kx[0]++;
-                }
             }
-            //return res.stream();
-        });/*.collect(Collectors.toList());*/
+        });
 
         out.close();
     }
+
+
+    private static void compareAndCopy(List<Name> names, int index, int jakkardIndex) throws IOException {
+
+        System.out.println("\nComparing...");
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("result.csv", true)));
+
+        names.forEach(name1 -> {
+            List<Result> res = new ArrayList<>();
+
+            int i = names.indexOf(name1);
+
+            if (!name1.isDone()) {
+
+                try {
+                    List<Name> toCopy = new ArrayList<>();
+                    toCopy.add(name1);
+
+                    Set<Long> s1Set;
+                    if (index > SHINGLE_MAP_THRESHOLD) {
+                        s1Set = SHINGLE_MAP.get(index).get(name1.getName());
+                    } else {
+                        s1Set = readShdFromFile(new File(SAMPLE_DIRS_MAP.get(index).getAbsolutePath() + File.separator + name1.getName() + ".shg"));
+                    }
+
+                    if (i != names.size() - 1) {
+                        for (int j = i + 1; j < names.size(); j++) {
+
+                            Name name2 = names.get(j);
+
+                            if (name2.isDone()) {
+                                continue;
+                            }
+
+                            Set<Long> s2Set;
+                            if (index > SHINGLE_MAP_THRESHOLD) {
+                                s2Set = SHINGLE_MAP.get(index).get(name2.getName());
+                            } else {
+                                s2Set = readShdFromFile(new File(SAMPLE_DIRS_MAP.get(index).getAbsolutePath() + File.separator + name2.getName() + ".shg"));
+                            }
+
+                            Set<Long> s1intersect = new HashSet<>(s1Set);
+                            Set<Long> s1union = new HashSet<>(s1Set);
+
+                            s1intersect.retainAll(s2Set);
+                            s1union.addAll(s2Set);
+                            double relative = s1intersect.size() * 100.0 / s1Set.size();
+                            double jakkard = s1intersect.size() * 100.0 / s1union.size();
+
+                            Result result = new Result(name1.getName(), name2.getName(), relative, jakkard);
+
+                            if (jakkard > jakkardIndex) { // 35
+                                name2.setDone(true);
+                                System.out.println(result);
+                                toCopy.add(name2);
+                            }
+
+                            res.add(result);
+                            out.println(result.toString());
+                            out.flush();
+                        }
+                    }
+                    name1.setDone(true);
+
+                    String catName = getCleanTitle(toCopy);
+
+                    File cat = Paths.get("D:\\Downloads\\games\\gata", catName).toFile();
+
+                    if (cat.exists()) {
+                        catName = getTitle(toCopy);
+                        cat = Paths.get("D:\\Downloads\\games\\gata", catName).toFile();
+                    }
+
+                    cat.mkdir();
+                    for (Name name : toCopy) {
+                        Files.move(name.getFile().toPath(), Paths.get("D:\\Downloads\\games\\gata", catName, name.getFile().getName()));
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                System.out.println("Skipping: " + name1.getName());
+            }
+        });
+
+        out.close();
+    }
+
 
     private static String getTitle(List<Name> names) {
         return names.stream().max(Comparator.comparing(Name::getIndex)).orElse(null).getName();
@@ -220,26 +306,44 @@ public class Main1024 {
         return names.stream().max(Comparator.comparing(Name::getIndex)).orElse(null).getCleanName();
     }
 
-    @SuppressWarnings("unchecked")
-    private static Map<String, Set<Long>> readShingleMapFromFile(File file) throws IOException, ClassNotFoundException {
-        FileInputStream fis = new FileInputStream(file);
-        ObjectInputStream ois = new ObjectInputStream(fis);
-        Map<String, Set<Long>> shinglesMap = (Map<String, Set<Long>>) ois.readObject();
-
-        ois.close();
-        fis.close();
-        return shinglesMap;
+    private static void serialize(File file, Object object) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+            oos.writeObject(object);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    //TODO unify
     @SuppressWarnings("unchecked")
-    private static Set<Long> readShdFromFile(File file) throws IOException, ClassNotFoundException {
-        FileInputStream fis = new FileInputStream(file);
-        ObjectInputStream ois = new ObjectInputStream(fis);
-        Set<Long> result = (Set<Long>) ois.readObject();
+    private static Map<String, Set<Long>> readShingleMapFromFile(File file) {
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Map<String, Set<Long>> shinglesMap = (Map<String, Set<Long>>) ois.readObject();
 
-        ois.close();
-        fis.close();
-        return result;
+            ois.close();
+            fis.close();
+            return shinglesMap;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //TODO unify
+    @SuppressWarnings("unchecked")
+    private static Set<Long> readShdFromFile(File file) {
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Set<Long> result = (Set<Long>) ois.readObject();
+
+            ois.close();
+            fis.close();
+            return result;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @SuppressWarnings("all")
